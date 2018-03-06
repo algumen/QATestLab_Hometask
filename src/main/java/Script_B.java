e92cce561c2acca4bd76ff919f087444d8e492f3
algumen <algumen@gmail.com>
2018-03-06 09:42:37 +0200

    Script B renew

--- src/main/java/Script_B.java
+++ src/main/java/Script_B.java
@@ -41,7 +41,7 @@ public class Script_B {
     checkLeftSideMenu(driver, "//li[@id='subtab-AdminParentOrders']", "//*[@class='page-title']");
 
 //        CATALOG
-    checkLeftSideMenu(driver, "//*[@id=\"subtab-AdminCatalog\"]", "//*[@class='title']");
+    checkLeftSideMenu(driver, "//*[@id='subtab-AdminCatalog']", "//*[@id='main-div']/div[1]/h2");
 
 
 
