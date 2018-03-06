b4f7f5b47754ce614fdfd26e8a83da7cbfbb9813
algumen <algumen@gmail.com>
2018-03-06 06:02:23 +0200

    Script B renew

--- src/main/java/Script_B.java
+++ src/main/java/Script_B.java
@@ -3,7 +3,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import java.util.concurrent.TimeUnit;
+
+import java.util.List;
 
 
 public class Script_B {
@@ -11,10 +12,8 @@ public class Script_B {
         WebDriver driver = initChromeDriver();
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
 
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (Exception e) {
-        }
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
 
         WebElement emailInput = driver.findElement(By.id("email"));
         emailInput.click();
@@ -26,24 +25,121 @@ public class Script_B {
         passwordInput.clear();
         passwordInput.sendKeys("Xcg7299bnSmMuRLp9ITw");
 
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
         WebElement submitLoginBtn = driver.findElement(By.name("submitLogin"));
         submitLoginBtn.click();
 
-        try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (Exception e) {
-        }
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
 
-        WebElement profileImg = driver.findElement(By.xpath("//img[(@class='imgm img-thumbnail')]"));
-        profileImg.click();
+//        DASHBOARD
 
-        try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (Exception e) {
-        }
+        WebElement dashBoardBtn = driver.findElement(By.xpath("//li[@id='tab-AdminDashboard']"));
+        dashBoardBtn.click();
 
-        WebElement logoutBtn = driver.findElement(By.id("header_logout"));
-        logoutBtn.click();
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        WebElement titleSubMenu = driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div/h2"));
+        String titleSubMenuText = titleSubMenu.getText();
+        System.out.println(titleSubMenuText);
+
+        driver.navigate().refresh();
+
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        WebElement titleSubMenu_new = driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div/h2"));
+        String titleSubMenuNewTextText = titleSubMenu_new.getText();
+
+        if (titleSubMenuNewTextText.equals(titleSubMenuText)) {
+            System.out.println(titleSubMenuText + "   is equial  " + titleSubMenuNewTextText);
+        } else {System.out.println(titleSubMenuText + "   is NOT equial   " + titleSubMenuNewTextText);}
+
+//        ORDERS
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        WebElement ordersBtn = driver.findElement(By.xpath("//*[@id=\"subtab-AdminParentOrders\"]/a"));
+        ordersBtn.click();
+
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        titleSubMenu = driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div/h2"));
+        titleSubMenuText = titleSubMenu.getText();
+        System.out.println(titleSubMenuText);
+
+        driver.navigate().refresh();
+
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        titleSubMenu_new = driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div/h2"));
+        titleSubMenuNewTextText = titleSubMenu_new.getText();
+
+        if (titleSubMenuNewTextText.equals(titleSubMenuText)) {
+            System.out.println(titleSubMenuText + "   is equial  " + titleSubMenuNewTextText);
+        } else {System.out.println(titleSubMenuText + "   is NOT equial   " + titleSubMenuNewTextText);}
+
+//        CATALOG
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        WebElement catalogBtn = driver.findElement(By.xpath("//*[@id=\"subtab-AdminCatalog\"]/a"));
+        catalogBtn.click();
+
+        try {Thread.sleep(2000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        titleSubMenu = driver.findElement(By.xpath("//*[@id=\"main-div\"]/div[1]/h2"));
+        titleSubMenuText = titleSubMenu.getText();
+        System.out.println(titleSubMenuText);
+
+        driver.navigate().refresh();
+
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        titleSubMenu_new = driver.findElement(By.xpath("//*[@id=\"main-div\"]/div[1]/h2"));
+        titleSubMenuNewTextText = titleSubMenu_new.getText();
+
+        if (titleSubMenuNewTextText.equals(titleSubMenuText)) {
+            System.out.println(titleSubMenuText + "   is equial  " + titleSubMenuNewTextText);
+        } else {System.out.println(titleSubMenuText + "   is NOT equial   " + titleSubMenuNewTextText);}
+
+//        CUSTOMERS
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        WebElement customersBtn = driver.findElement(By.xpath("//*[@id=\"subtab-AdminParentCustomer\"]/a/span"));
+        customersBtn.click();
+
+        try {Thread.sleep(2000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        titleSubMenu = driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div/h2"));
+        titleSubMenuText = titleSubMenu.getText();
+        System.out.println(titleSubMenuText);
+
+        driver.navigate().refresh();
+
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
+
+        titleSubMenu_new = driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div/h2"));
+        titleSubMenuNewTextText = titleSubMenu_new.getText();
+
+        if (titleSubMenuNewTextText.equals(titleSubMenuText)) {
+            System.out.println(titleSubMenuText + "   is equial  " + titleSubMenuNewTextText);
+        } else {System.out.println(titleSubMenuText + "   is NOT equial   " + titleSubMenuNewTextText);}
+
+
+
+        try {Thread.sleep(1000);}
+        catch (InterruptedException e) {e.printStackTrace();}
 
         driver.quit();
 
