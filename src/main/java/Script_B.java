c9e8123618c946ce3f290abdbc6208cd2c7dc311
algumen <algumen@gmail.com>
2018-03-06 09:56:29 +0200

    Script B renew

--- src/main/java/Script_B.java
+++ src/main/java/Script_B.java
@@ -43,32 +43,10 @@ public class Script_B {
 //        CATALOG
     checkLeftSideMenu(driver, "//*[@id='subtab-AdminCatalog']", "//*[@id='main-div']/div[1]/h2");
 
+//        CUSTOMERS //*[@id='subtab-AdminParentCustomer']
+checkLeftSideMenu(driver, "//li[5]/a/span", "//*[@class='page-title']");
 
 
-//        try {Thread.sleep(1000);}
-//        catch (InterruptedException e) {e.printStackTrace();}
-//
-//        WebElement catalogBtn = driver.findElement(By.xpath("//*[@id=\"subtab-AdminCatalog\"]/a"));
-//        catalogBtn.click();
-//
-//        try {Thread.sleep(2000);}
-//        catch (InterruptedException e) {e.printStackTrace();}
-//
-//        titleSubMenu = driver.findElement(By.xpath("//*[@id=\"main-div\"]/div[1]/h2"));
-//        titleSubMenuText = titleSubMenu.getText();
-//        System.out.println(titleSubMenuText);
-//
-//        driver.navigate().refresh();
-//
-//        try {Thread.sleep(1000);}
-//        catch (InterruptedException e) {e.printStackTrace();}
-//
-//        titleSubMenu_new = driver.findElement(By.xpath("//*[@id=\"main-div\"]/div[1]/h2"));
-//        titleSubMenuNewTextText = titleSubMenu_new.getText();
-//
-//        if (titleSubMenuNewTextText.equals(titleSubMenuText)) {
-//            System.out.println(titleSubMenuText + "   is equial  " + titleSubMenuNewTextText);
-//        } else {System.out.println(titleSubMenuText + "   is NOT equial   " + titleSubMenuNewTextText);}
 
 //        CUSTOMERS
 //        try {Thread.sleep(1000);}
